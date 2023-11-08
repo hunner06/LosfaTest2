a4e403e9eccd85d0b6e52d2117b80eb64e463972
Hunter Guest <Hguest@antaresnet.com>
2023-11-07 18:18:40 -0600

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { StoreService } from './services/store.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'store-service-app';
+  constructor(private storeService: StoreService) {
+   
+  }
+
+  ngOnInit() {
+  
+    }
 }
